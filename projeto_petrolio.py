ae8cd035c4f9830d54a49a5a5e0a918d27f7f4a1
WelingtonASousa <111813138+WelingtonASousa@users.noreply.github.com>
2024-05-19 21:28:32 -0300

    Update projeto_petrolio.py

--- projeto_petrolio.py
+++ projeto_petrolio.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1LElorRTEl3POLHPo_U26Cuk_ON7uOC9g
 """
 
-!pip install prophet
+#!pip install prophet
 
 !pip install streamlit
 !pip instal yfinance
