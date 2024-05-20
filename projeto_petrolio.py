85ffd3db9936c1110da6e726e771ada2b9455d1a
WelingtonASousa <111813138+WelingtonASousa@users.noreply.github.com>
2024-05-19 21:34:11 -0300

    Update projeto_petrolio.py

--- projeto_petrolio.py
+++ projeto_petrolio.py
@@ -10,7 +10,7 @@ Original file is located at
 # pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
-import yfinance as yf
+
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
