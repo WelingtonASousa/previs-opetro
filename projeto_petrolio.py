00092882a7ba47310112e31ad82937ad57d094cb
WelingtonASousa <111813138+WelingtonASousa@users.noreply.github.com>
2024-05-19 21:32:44 -0300

    Update projeto_petrolio.py

--- projeto_petrolio.py
+++ projeto_petrolio.py
@@ -7,45 +7,40 @@ Original file is located at
     https://colab.research.google.com/drive/1LElorRTEl3POLHPo_U26Cuk_ON7uOC9g
 """
 
-#!pip install prophet
-#!pip install streamlit
-#!pip instal yfinance
-#!pip install plotly
-
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
 
 import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-from prophet import Prophet
-
-from prophet.plot import plot_plotly
-
-START = "2022-01-01"
+START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title('Previsão do Preço do Petróleo')
+st.title('Stock Forecast App')
 
-stocks = ('BZ=F')
-selected_stock = st.selectbox(' Preço do Petrólio', stocks)
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
+
+@st.cache
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
 
-data_load_state = st.text('carreegando dados...')
+	
+data_load_state = st.text('Loading data...')
 data = load_data(selected_stock)
-data_load_state.text('Pronto')
+data_load_state.text('Loading data... done!')
 
 st.subheader('Raw data')
 st.write(data.tail())
-
-"""# Plot raw data"""
 
 # Plot raw data
 def plot_raw_data():
@@ -54,7 +49,7 @@ def plot_raw_data():
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
 	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
-
+	
 plot_raw_data()
 
 # Predict forecast with Prophet.
@@ -69,7 +64,7 @@ forecast = m.predict(future)
 # Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast.tail())
-
+    
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
